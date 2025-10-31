73448dcedc1f7f7b51f9ed898109393594040e00
Maynard Peralta <maynperalta@gmail.com>
2025-10-30 22:10:30 -0600

    Added switch case

--- Week8Assignment/src/main/Main.java
+++ Week8Assignment/src/main/Main.java
@@ -1,10 +1,54 @@
 package main;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+	
+	private List<Automobile> inventory = new ArrayList<>();
+	private static final String FILE_PATH = "";
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Main app = new AutomobileInventory();
+		app.run();
 	}
 
+	public void run() {
+		loadFromFile();
+		try(Scanner scnr = new Scanner(System.in)) {
+			boolean running = true;
+			
+			while (running) {
+				System.out.println("Menu:");
+				System.out.println("1. Add vehicle");
+				System.out.println("2. Display inventory");
+				System.out.println("3. Update vehicle");
+				System.out.println("4. Delete vehicle");
+				System.out.println("5. Save changes");
+				System.out.println("6. Exit");
+				System.out.println("Select an option: ");
+				
+				String userSelection = scnr.nextLine();
+				
+				switch (userSelection) {
+					case "1" -> addAutomobile(scnr);
+					case "2" -> displayInventory();
+					case "3" -> updateAutomobile(scnr);
+					case "4" -> deleteAutomobile(scnr);
+					case "5" -> saveToFile();
+					case "6" -> {
+						saveToFile();
+						System.out.println("Saving inventory and exiting program.");
+						running = false;
+					}
+					default -> System.out.println("Invalid selection. Try again.");				
+				}
+			}
+		} catch (Exception e){
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+	
 }
